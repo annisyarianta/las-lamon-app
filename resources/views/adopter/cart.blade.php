012c38c10359985822b4f4dca942dc6bded405a8
putriiiiy <putriindriyanti190203@gmail.com>
2026-04-20 15:25:40 +0700

    update cart

--- resources/views/adopter/cart.blade.php
+++ resources/views/adopter/cart.blade.php
@@ -28,68 +28,76 @@
                     </thead>
 
                     <tbody>
-                        @foreach ($data as $each_data)
+                        @if (empty($data) || count($data) == 0)
                             <tr>
-
-                                <td>
-                                    <p class="mb-0 mt-4">
-                                        {{ ucwords(str_replace('_', ' ', $each_data->katalog->nama_katalog ?? '-')) }}
-                                    </p>
+                                <td colspan="6" class="text-center py-5">
+                                    <h4 class="text-secondary">Your cart is empty.</h4>
                                 </td>
-
-                                <td>
-                                    <p class="mb-0 mt-4">
-                                        {{ $each_data->produk->nama_produk ?? '-' }}
-                                    </p>
-                                </td>
-
-                                <td>
-                                    <p class="mb-0 mt-4">
-                                        Rp {{ number_format($each_data->harga_satuan, 0, ',', '.') }}
-                                    </p>
-                                </td>
-
-                                <td class="produk-data" data-id="{{ $each_data->produk->id ?? null }}"
-                                    data-id-cart="{{ $each_data->id }}" data-id-katalog="{{ $each_data->katalog->id }}"
-                                    data-harga="{{ $each_data->harga_satuan }}"
-                                    data-nama-katalog="{{ $each_data->katalog->nama_katalog ?? null }}"
-                                    data-nama-produk="{{ $each_data->produk->nama_produk ?? null }}">
-
-                                    <div class="input-group quantity mt-4" style="width: 100px;">
-                                        <button type="button" class="btn btn-sm btn-minus bg-light border">-</button>
-
-                                        <input type="text"
-                                            class="form-control form-control-sm text-center border-0 qty-input"
-                                            value="{{ $each_data->kuantitas }}">
-
-                                        <button type="button" class="btn btn-sm btn-plus bg-light border">+</button>
-                                    </div>
-                                </td>
-
-                                <td>
-                                    <p class="mb-0 mt-4 total-harga">
-                                        Rp
-                                        {{ number_format($each_data->harga_satuan * $each_data->kuantitas, 0, ',', '.') }}
-                                    </p>
-                                </td>
-
-                                <td>
-                                    <form
-                                        action="{{ route('adopter.cart.destroy', ['id' => Crypt::encrypt($each_data->id)]) }}"
-                                        method="POST" style="display:inline;">
-
-                                        @csrf
-                                        @method('DELETE')
-
-                                        <button type="submit" class="btn btn-md rounded-circle bg-light border mt-4">
-                                            <i class="fa fa-trash text-danger"></i>
-                                        </button>
-
-                                    </form>
-                                </td>
-
                             </tr>
-                        @endforeach
+                        @else
+                            @foreach ($data as $each_data)
+                                <tr>
+
+                                    <td>
+                                        <p class="mb-0 mt-4">
+                                            {{ ucwords(str_replace('_', ' ', $each_data->katalog->nama_katalog ?? '-')) }}
+                                        </p>
+                                    </td>
+
+                                    <td>
+                                        <p class="mb-0 mt-4">
+                                            {{ $each_data->produk->nama_produk ?? '-' }}
+                                        </p>
+                                    </td>
+
+                                    <td>
+                                        <p class="mb-0 mt-4">
+                                            Rp {{ number_format($each_data->harga_satuan, 0, ',', '.') }}
+                                        </p>
+                                    </td>
+
+                                    <td class="produk-data" data-id="{{ $each_data->produk->id ?? null }}"
+                                        data-id-cart="{{ $each_data->id }}" data-id-katalog="{{ $each_data->katalog->id }}"
+                                        data-harga="{{ $each_data->harga_satuan }}"
+                                        data-nama-katalog="{{ $each_data->katalog->nama_katalog ?? null }}"
+                                        data-nama-produk="{{ $each_data->produk->nama_produk ?? null }}">
+
+                                        <div class="input-group quantity mt-4" style="width: 100px;">
+                                            <button type="button" class="btn btn-sm btn-minus bg-light border">-</button>
+
+                                            <input type="text"
+                                                class="form-control form-control-sm text-center border-0 qty-input"
+                                                value="{{ $each_data->kuantitas }}">
+
+                                            <button type="button" class="btn btn-sm btn-plus bg-light border">+</button>
+                                        </div>
+                                    </td>
+
+                                    <td>
+                                        <p class="mb-0 mt-4 total-harga">
+                                            Rp
+                                            {{ number_format($each_data->harga_satuan * $each_data->kuantitas, 0, ',', '.') }}
+                                        </p>
+                                    </td>
+
+                                    <td>
+                                        <form
+                                            action="{{ route('adopter.cart.destroy', ['id' => Crypt::encrypt($each_data->id)]) }}"
+                                            method="POST" style="display:inline;">
+
+                                            @csrf
+                                            @method('DELETE')
+
+                                            <button type="submit" class="btn btn-md rounded-circle bg-light border mt-4">
+                                                <i class="fa fa-trash text-danger"></i>
+                                            </button>
+
+                                        </form>
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
