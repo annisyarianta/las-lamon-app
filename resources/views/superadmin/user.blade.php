c0083521414985932c6c83b44ed6cb9a37812bfd
pututiaraa <niputu222222@gmail.com>
2026-04-23 22:08:03 +0700

    update controller & model dashboard superadmin

--- resources/views/superadmin/user.blade.php
+++ resources/views/superadmin/user.blade.php
@@ -8,7 +8,7 @@
         <div class="container py-5">
             <div class="d-flex justify-content-between align-items-center mt-5 mb-3 flex-wrap gap-2">
                 <h5 class="mb-0 fw-semibold">User Lists</h5>
-                <a href="#" class="btn btn-dark d-flex align-items-center gap-1">
+                <a href="{{ route('users.create') }}" class="btn btn-dark d-flex align-items-center gap-1">
                     <i class="fas fa-plus"></i> Create User
                 </a>
             </div>
@@ -25,34 +25,44 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1. </td>
-                            <td>Ni Putu Tiara</td>
-                            <td>tiara@mail.com</td>
-                            <td>08xxxxxxxxxx</td>
-                            <td>
-                                <span class="badge bg-light text-primary border border-primary">
-                                    Adopter
-                                </span>
-                            </td>
-
-                            <!-- Action -->
-                            <td class="text-center">
-                                <div class="d-flex flex-column flex-md-row justify-content-center gap-1">
-
-                                    <a href="{{ route('edit_user') }}"
-                                        class="btn btn-primary
-                                          px-2 py-1 px-md-3 py-md-2 small">
-                                        <i class="fa fa-pen me-1"></i>
-                                        <span class="d-none d-md-inline">Edit</span>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td> {{ $user->name }} </td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->phone }}</td>
+                                <td>
+                                    <span class="badge b{{ $user->role_badge }}">
+                                        {{ ucfirst($user->role) }}
+                                    </span>
+                                </td>
+                                <td class="text-center">
+                                    <div class="d-flex flex-column flex-md-row justify-content-center gap-1">
+                                        <a href=" {{ route('users.edit', $user->id) }} "
+                                            class="btn btn-primary p-1 px-md-3 py-md-2 small">
+                                            <i class="fa fa-pen me-1"></i>
+                                            <span class="d-none d-md-inline">Edit</span>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 
+@if(session('success'))
+<script>
+    Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: '{{ session('success') }}',
+        confirmButtonColor: '#114D14'
+    });
+</script>
+@endif
+
 @endsection
+
